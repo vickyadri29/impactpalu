545781dd93e3489f24e0ca129ca7e9cf67049900
vickyadri29 <vickyadri290501@gmail.com>
2022-09-27 00:10:04 +0800

    fix some bug

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,7 @@ import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap');
-        </style>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,15 @@ export default function Home() {
     <>
       <Head>
         <title>Welcome to Impact Palu!</title>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
+        />
       </Head>
       <div className="font-poppins">
         <Navbar />
