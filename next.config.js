2210f9dab3fe756b01d0c6481057c30752ac0766
afandy2000 <afandywibowo2000@gmail.com>
2022-10-02 21:17:15 +0800

    feat: add export to html

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: './'
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
